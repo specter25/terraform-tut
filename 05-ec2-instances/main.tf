11cb23ec5f9ae467b6e385fa1de2fb4142053c8e
specter25 <ujjwalcoding012@gmail.com>
2020-08-08 00:30:41 +0530

    connections and execution scripts added

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,3 +1,6 @@
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/default-ec2.pem"
+}
 
 provider "aws" {
   region  = "us-east-1"
@@ -30,4 +33,28 @@ resource "aws_security_group" "http-server-sg" {
   tags = {
     name = "http-server-sg"
   }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-02354e95b39ca8dec"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http-server-sg.id]
+  subnet_id              = "subnet-06ac7727"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to virtual service ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
+
 }
