58d190076d89de0603d6a03632337c0fc41a2eb8
specter25 <ujjwalcoding012@gmail.com>
2020-08-08 12:38:37 +0530

    default vpc resource added

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -7,10 +7,14 @@ provider "aws" {
   version = "~> 2.46"
 }
 
+resource "aws_default_vpc" "default" {
+
+}
+
 # http server security group , expose 80 tcp for http , 22 tcp for ssh and cidr(ip address allowed)["0.0.0.0/0"]
 resource "aws_security_group" "http-server-sg" {
   name   = "http_server_sg"
-  vpc_id = "vpc-9f3fd6e2"
+  vpc_id = aws_default_vpc.default.id
 
   ingress {
     from_port   = 80
