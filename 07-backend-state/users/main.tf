048c9fc836960762f22bbe77360e1d64b833e8d5
specter25 <ujjwalcoding012@gmail.com>
2020-08-09 23:08:30 +0530

    07 remote backend connected

--- 07-backend-state/users/main.tf
+++ 07-backend-state/users/main.tf
@@ -1,3 +1,25 @@
+variable application_name {
+    default = "07-backend-state"
+}
+variable project_name {
+    default = "users"
+}
+variable environmment {
+    default = "dev"
+}
+
+terraform {
+    backend "s3" {
+        bucket="dev-application-name-backend-state-63874060"
+        key = "07-backend-state-users-dev"
+        region="us-east-1"
+        dynamodb_table="dev-application-locks"
+        encrypt=true
+    }
+    
+
+}
+
 provider "aws" {
     region = "us-east-1"
     version= "~> 2.46"
--- 07-backend-state/users/outputs.tf
+++ 07-backend-state/users/outputs.tf
@@ -1,4 +1,4 @@
 
 output "my_iam_user_terra_arn" {
-    value = aws_iam_user.my_iam_user_terra.arn
+  value = aws_iam_user.my_iam_user_terra.arn
 }
